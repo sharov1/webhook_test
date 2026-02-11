92c6f9674717f2cf4f7cd1a5019cdd6384a5eb6d
sharov1 <dr.sharov2011@gmail.com>
2026-02-12 02:03:09 +0400

    reciver added

--- app.py
+++ app.py
@@ -1,25 +1,56 @@
 from fastapi import FastAPI, Request
-import json
 from datetime import datetime
+import httpx
 
 app = FastAPI()
 
+TARGET_URL = "https://webhook.site/e11323ff-37b1-4f16-87cf-4c9c467b8bb4"
+
 def now():
-    return datetime.utcnow().isoformat()
+    return datetime.utcnow().isoformat() + "Z"
+
+async def send_to_api(data: dict):
+    async with httpx.AsyncClient(timeout=5) as client:
+        response = await client.post(TARGET_URL, json=data)
+        response.raise_for_status()
 
 @app.post("/webhook")
 async def receive_webhook(request: Request):
-    # читаем JSON из тела запроса
     payload = await request.json()
-
-    # заголовок с типом события GitHub
     event_type = request.headers.get("X-GitHub-Event")
 
-    print("\n================ WEBHOOK RECEIVED ================\n")
-    print("recived at -", now())
-    print(f"Event type: {event_type}")
-    print("Payload:")
-    print(json.dumps(payload, indent=2))
-    print("\n==================================================\n")
+    outgoing_payload = None
+
+    if event_type == "deployment":
+        deployment = payload.get("deployment", {})
+
+        outgoing_payload = {
+            "source": "github",
+            "event": "deployment_started",
+            "deployment_id": deployment.get("id"),
+            "environment": deployment.get("environment"),
+            "status": "in_progress",
+            "timestamp": deployment.get("created_at") or now()
+        }
+
+    elif event_type == "deployment_status":
+        deployment = payload.get("deployment", {})
+        status = payload.get("deployment_status", {})
+
+        outgoing_payload = {
+            "source": "github",
+            "event": "deployment_finished",
+            "deployment_id": deployment.get("id"),
+            "environment": deployment.get("environment"),
+            "status": status.get("state"),
+            "timestamp": status.get("created_at") or now()
+        }
+
+    if outgoing_payload:
+        try:
+            await send_to_api(outgoing_payload)
+            print("➡️ Sent to external API:", outgoing_payload)
+        except Exception as e:
+            print("❌ Failed to send webhook:", str(e))
 
     return {"status": "ok"}
